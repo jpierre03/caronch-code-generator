2a1df3fb3310bbba0c0672c8cdf546df35303e59
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-10-25 22:56:47 +0100

    add Parser

--- src/Caronch/Lang/Data.hs
+++ src/Caronch/Lang/Data.hs
@@ -4,14 +4,13 @@ type Caronch = [Item]
 
 data Item   = SimpleProcess Id
 --            | LabeldProcess Id Label
-            | Process Id Label Name
+--            | Process Id Label Name
             | SimpleData Id
 --            | LabeldData Id Label
-            | Data Id Label Name
+--            | Data Id Label Name
             | Link Id Id
             deriving (Show)
 
+type Id = String
 type Label = String
-type Name = Nme String
-
-newtype Id = Id String deriving (Eq, Ord, Show, Read)
+type Name = String
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,52 @@
 module Main where
 
+import          Caronch.Lang.Data
+import          Caronch.Lang.Parser
+
+import           Data.Either
+import           Data.List                        hiding (find)
+import           Data.List.Split
+import           System.Directory
+import           System.Environment
+import           System.FilePath.Find
+import           System.IO
+import           Text.Parsec
+
+pattern :: String
+pattern = "*.caronch"
+
 main :: IO ()
-main = putStrLn "Hello !"
+main = do
+    dir   <- getCurrentDirectory
+    files <- search dir
+    content <- mapM readFile files
+    let normalisedContent = map normaliseComments content
+        objects = zipWith parseConfigFile files normalisedContent
+    let all = zip files objects
+        errors = filter (\ (_,x) -> isLeft x) all
+        goods = filter (\ (_,x) -> isRight x) all
+    mapM_ printBoth errors
+    putStrLn "* Stats"
+    putStr "    * Files found: "
+    print $ length files
+    putStr "    * Objects found: "
+    print . length . concat . rights $ objects
+    putStr "    * Files with errors: "
+    print $ length errors
+    --printArray 1 content
+    putStrLn "Hello !"
+
+search :: FilePath -> IO [FilePath]
+search = find always ( fileName ~~? pattern )
+
+normaliseComments :: String -> String
+normaliseComments = map (\ x -> if x == ';' then '#' else x)
+
+printBoth :: FilePath -> Either ParseError [Item] -> IO ()
+printBoth filepath item = do
+    putStr "***** File: "
+    putStrLn filepath
+    print item
+    putStrLn ""
+
+printArray n arr = mapM_ (putStrLn . unwords) $ map (map show) $ chunksOf n arr
