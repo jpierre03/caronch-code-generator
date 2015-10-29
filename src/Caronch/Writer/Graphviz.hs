a2b46b1be0f1437f53a06eb98b9b7ccfa9adb42f
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-10-29 21:31:51 +0100

    add gv support for Simple* datastructure

--- src/Caronch/Writer/Graphviz.hs
+++ src/Caronch/Writer/Graphviz.hs
@@ -3,4 +3,28 @@ module Caronch.Writer.Graphviz where
 import          Caronch.Lang.Data
 
 writeGraphviz :: [Item] -> String
-writeGraphviz items = "toto"
+writeGraphviz items = begin_gv ++ body_gv ++ end_gv
+    where body_gv = concatMap writeGraphviz' items
+          begin_gv   = "digraph Caronch {" ++ eol ++ comments ++ eol
+          end_gv     = "}" ++ eol
+          comments   = unlines $
+                        [ "//"
+                        , "// This model is generated through CARONCH formalism."
+                        , "// To create a picture from this model, you need Graphviz :"
+                        , "// dot caronch.gv -Tpng -o caronch.png"
+                        , "//"
+                        , ""
+                        ]
+          eol = "\n"
+
+writeGraphviz' :: Item -> String
+writeGraphviz' item =
+    case item of
+        (SimpleData  id)    -> color_data ++ "data_"  ++ id ++ eol
+        (SimpleProcess id)  -> color_process ++ "process_" ++ id ++ eol
+        (Link id id')       -> id ++ " -> " ++ id' ++ eol
+        _ -> "_ "
+    where eol =";\n"
+          color_data="node [shape=box,style=\"filled\",fillcolor=peachpuff]" ++ eol
+          color_process="node [shape=ellipse,style=\"filled\",fillcolor=olivedrab1]" ++ eol
+
